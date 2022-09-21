8452473cab96c58da02426e0a724588df94d5411
stecarraa <stefano@caratozzolo.me>
2022-09-21 15:51:44 +0200

    init nuova repo

--- public/js/app.js
+++ public/js/app.js
@@ -49681,8 +49681,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/stecarraa/Documents/Boolean-Repo/laravel-auth/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/stecarraa/Documents/Boolean-Repo/laravel-auth/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/stecarraa/Documents/Boolean-Repo/laravel-one-to-many/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/stecarraa/Documents/Boolean-Repo/laravel-one-to-many/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
